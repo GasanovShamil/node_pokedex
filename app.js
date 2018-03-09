d25f5f1ca081039bfca6e13044ac393159e690dd
GasanovShamil <gasanov.sh@gmail.com>
2018-03-09 13:08:05 +0100

    cloud swagger fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var swaggerDefinition = {
         version: '1.0.0',
         description: 'Demonstrating how to describe a RESTful API with Swagger',
     },
-    host: 'http://pokedex.gasanov.fr',
+    host: 'pokedex.gasanov.fr',
     basePath: '/',
 };
 
